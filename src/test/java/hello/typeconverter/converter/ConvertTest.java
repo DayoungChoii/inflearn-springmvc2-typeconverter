12f563038350c5dacaf42ffebcd397c8f24c8e56
DayoungChoii <young12430@gmail.com>
2022-06-08 12:43:40 +0900

    [springmvc2] 타입 컨버터 - Converter

---
+++ src/test/java/hello/typeconverter/converter/ConvertTest.java
@@ -0,0 +1,38 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConvertTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIp() {
+        StringToIpConverter converter = new StringToIpConverter();
+        IpPort result = converter.convert("127.0.0.1:8080");
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    void IptoString() {
+        IpToStringConverter converter = new IpToStringConverter();
+        String result = converter.convert(new IpPort("127.0.0.1", 8080));
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+}
